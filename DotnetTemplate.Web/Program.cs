b097ea089d550827e2af8b856061f87bdf6b6b0c
Kingsley <kingbrookeyyahoo.com  git config --global user.name Kingsley>
2023-11-22 14:16:57 +0000

    Updated running port from 5000 to 5001

--- DotnetTemplate.Web/Program.cs
+++ DotnetTemplate.Web/Program.cs
@@ -32,7 +32,7 @@ namespace DotnetTemplate.Web
                 .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => 
                 {
-                    var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+                    var port = Environment.GetEnvironmentVariable("PORT") ?? "5001";
                     webBuilder.UseStartup<Startup>().UseUrls("http://*:" + port);
                 });
         }
